839c847ac83999a4fbc0f457a5bc13c7fab86ce7
Anatoly Politsin <politsin@gmail.com>
2016-09-02 18:01:03 +0300

    bad

--- init.pl
+++ init.pl
@@ -5,15 +5,21 @@ use warnings;
 
 my $passwd  = $ARGV[0];
 my $docker  = '';
+my $keys    = '/opt/docker-rest/ssl/proxy.crt';
 
 my $docker_check = 'dpkg -l | grep -c docker-engine';
 chomp($docker_check);
 
-if ($passwd){
+if (1){
   print "$passwd\n";
-  system("htpasswd -b /opt/docker-rest/.passwd synapse $passwd");
+  system("htpasswd -bc /opt/docker-rest/.passwd synapse $passwd");
   system("echo $passwd > /opt/docker-rest/passwd");
-  system("openssl req -x509 -nodes -days 9000 -newkey rsa:2048 -keyout /opt/docker-rest/ssl/proxy.key -out /opt/docker-rest/ssl/proxy.crt");
 }else{
-  'Need Passwd as ARGV[0]';
+  print "Need Passwd as ARGV[0]\n";
+}
+
+if(-e $keys){
+  print "Keys Exist\n";
+}else{
+  system("openssl req -x509 -nodes -days 9000 -newkey rsa:2048 -keyout /opt/docker-rest/ssl/proxy.key -out /opt/docker-rest/ssl/proxy.crt");
 }
