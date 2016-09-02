f6848cca6e2acb0f68dbbb4a13373ebe5cf96bac
Anatoly Politsin <politsin@gmial.com>
2016-09-02 21:18:15 +0300

    genKeys

---
+++ genKeys.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $keys    = '/opt/docker-rest/ssl/rest.crt';
+
+if(-e $keys){
+  print "Keys Exist\n";
+}else{
+  system("openssl req -x509 -nodes -days 9000 -newkey rsa:2048 -keyout /opt/docker-rest/ssl/rest.key -out /opt/docker-rest/ssl/rest.crt");
+  system("sed -i -e 's/example.crt/rest.crt/g' /opt/docker-rest/nginx.conf");
+  system("sed -i -e 's/example.key/rest.key/g' /opt/docker-rest/nginx.conf");
+  system("docker restart docker-rest");
+}
