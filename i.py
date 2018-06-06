4adb3c2a8a021a09f630e81a19b6f64e8b4ec4d1
Anatoly Politsin <politsin@gmail.com>
2018-06-06 11:10:54 +0300

    pl>py

--- i.py
+++ i.py
@@ -1,21 +1,46 @@
+#!/usr/bin/python3
+
 import sys
 import uuid
 import os
+import subprocess
+import platform
 
 #print 'Number of arguments:', len(sys.argv), 'arguments.'
 #print 'Argument List:', str(sys.argv)
+# Vars
+passw = ""
+docker = "docker-ce"
+compose = "1.21.2"
 
-passw = str(sys.argv[1])
-if len(passw) < 3:
+# Set Pass form ARG or GEN
+if 1 < len(sys.argv):
+    passw = str(sys.argv[1])
+if len(passw) < 5:
     uid = uuid.uuid4()
     passw = uid.hex
-print 'Pass=', passw
+print ('Pass=', passw)
 
-docker_check = os.system("dpkg -l | grep -c docker")
-docker_ver = os.system("docker --version")
-print docker_check, docker_ver
+docker_check = os.system('dpkg -l | grep -c %s' % (docker))
+if (docker_check):
+    os.system('apt-get update')
+    os.system('apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common')
+    os.system('curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -')
+    os.system('apt-key fingerprint 0EBFCD88')
+    ostype = platform.dist()[0]
+    deb = "https://download.docker.com/linux/%s $(lsb_release -cs) stable" % (ostype)
+    os.system('add-apt-repository "deb [arch=amd64] %s"' % (deb))
+    os.system('apt-get update')
+    os.system('apt-get install -y --force-yes docker-ce')
+    os.system("echo 'DOCKER_OPTS=\"-H unix:///var/run/docker.sock\"' >> /etc/default/docker")
+    os.system("service docker restart")
+    os.system("curl -L https://github.com/docker/compose/releases/download/%s/docker-compose-`uname -s`-`uname -m` > /usr/bin/docker-compose" % (compose))
+    os.system("chmod +x /usr/bin/docker-compose")
+    os.system("echo 'Europe/Moscow' > /etc/timezone")
+    os.system("htpasswd -bc /opt/docker-rest/.passwd synapse %s" % (passw))
+    os.system("echo %s > /opt/docker-rest/passwd" % (passw))
+    os.system("cd /opt/docker-rest && docker-compose up -d docker-rest")
 
-if docker_check == 1:
-    print 'Docker exist', os.system("docker --version")
 else:
-    print 'Need docker'
+    ver = os.system("docker --version")
+    print ('Docker exist', ver)
