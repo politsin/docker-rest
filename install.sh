271447cee6ae9be7097bb296ec5520cd9ba997a5
Leonid Razumov <masdzen@gmail.com>
2021-07-15 11:05:43 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,20 +6,24 @@ apt remove -y exim4-base exim4-config exim4-daemon-light
 apt upgrade -y
 apt autoremove -y
 apt install -y -qq \
-  apt-transport-https \
-  ca-certificates \
-  apache2-utils \
-  mc \
-  git \
-  htop \
-  curl \
-  python3 \
-  telegraf \
-  dnsutils \
-  net-tools \
-  inetutils-ping \
-  software-properties-common
+    apt-transport-https \
+    ca-certificates \
+    apache2-utils \
+    mc \
+    git \
+    zip \
+    htop \
+    curl \
+    ncdu \
+    unzip \
+    ranger \
+    python3 \
+    dnsutils \
+    net-tools \
+    inetutils-ping \
+    software-properties-common
 apt install -y -qq docker docker-compose
+apt install -y -qq telegraf
 cd /opt/docker-rest
 
 PASS=$(LC_ALL=C tr -dc '[:alnum:]' < /dev/urandom | head -c20)
